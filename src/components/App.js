a6a2a10d168f70420dcf31475ff0765ae685ea6f
sukantadas1 <sukantadas.das82@gmail.com>
2024-01-19 12:14:48 +0530

    submitting code-Fri Jan 19 2024 12:14:47 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
 import React from "react";
-import './../styles/App.css';
+import ClickCounter from "./components/ClickCounter";
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <h1>React Click Counter App</h1>
+      <ClickCounter />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/ClickCounter.js
@@ -0,0 +1,19 @@
+// src/components/ClickCounter.js
+import React, { useState } from "react";
+
+const ClickCounter = () => {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleButtonClick = () => {
+    setClickCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>Click me</button>
+      <p>Button clicked {clickCount} times.</p>
+    </div>
+  );
+};
+
+export default ClickCounter;
